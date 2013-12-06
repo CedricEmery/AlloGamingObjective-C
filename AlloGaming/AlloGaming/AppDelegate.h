5e7f9bc25cab38b6d6745c9bde81bde86f53e22a
CedricEmery <cedric.emery34@gmail.com>
2013-12-06 11:42:57 +0100

    Création du projet xcode

---
+++ AlloGaming/AlloGaming/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AlloGaming
+//
+//  Created by jean roget on 06/12/2013.
+//  Copyright (c) 2013 jean roget. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AlloGaming/AlloGaming/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlloGaming
+//
+//  Created by jean roget on 06/12/2013.
+//  Copyright (c) 2013 jean roget. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
